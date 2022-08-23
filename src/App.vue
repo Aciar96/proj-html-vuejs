8d13aea7ca98207334c168ec6fe20156e2d6f606
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-08-23 15:14:01 +0200

    add MainHeader

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div>
+    <MainHeader></MainHeader>
   </div>
 </template>
 
 <script>
+
+import MainHeader from "./components/MainHeader.vue"
+export default {
+  name: "App",
+  data() {
+  },
+  components: {
+   MainHeader,
+
+  },
+};
+  
 
 </script>
 
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <header >
+    <img src="../assets/img/dark-logo.png" alt="logo">
+    <ul>
+        <li>home</li>
+        <li>pages</li>
+        <li>courses</li>
+        <li>features</li>
+        <li>blog</li>
+        <li>shop</li>
+    </ul>
+    <ul>
+      <li><font-awesome-icon icon="fa-brands fa-twitter" /></li>
+      <li><i class="fa-brands fa-facebook-f"></i></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainHeader"
+  }
+</script>
+
+<style lang="scss" >
+header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+} 
+img{
+  height: 50px;
+    width: 150px;
+   
+}
+ul{
+  display: flex;
+}
+li{
+ list-style-type: none;
+ padding:  1rem; 
+}
+</style>
